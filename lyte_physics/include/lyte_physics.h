e30e73e4651d1add4e4b9dabd9f8c2497882990f
M <morew4rd@gmail.com>
2023-05-29 19:46:38 -0700

    v0.6-pre.4: prep

--- lyte_physics/include/lyte_physics.h
+++ lyte_physics/include/lyte_physics.h
@@ -7,13 +7,20 @@
 #include "api_generated.h" // types and enums
 
 
+
 typedef struct lyte_PhysicsState {
     lyte_PhysicsEngineState enginestate;
+    void *world;
+
 } lyte_PhysicsState;
 
 extern lyte_PhysicsState lytephysics_state;
 
+
 int lyte_physics_init(void);
 int lyte_physics_cleanup(void);
+
+
+int lyte_set_physics_engine(lyte_PhysicsEngineState state);
 
 #endif  // LYTE_PHYSICS_H_INCLUDED
--- lyte_physics/src/physics.c
+++ lyte_physics/src/physics.c
@@ -13,11 +13,35 @@ lyte_PhysicsState lytephysics_state = {0};
 
 int lyte_physics_init(void) {
     printf("Phys init\n");
-    dWorldCreate();
+    lytephysics_state.world = dWorldCreate();
+    lyte_set_physics_engine(LYTE_PHYSICSENGINESTATE_OFF);
     return 0;
 }
 
 int lyte_physics_cleanup(void) {
     printf("Phys cleanup\n");
+    if (lytephysics_state.world) {
+        dWorldDestroy(lytephysics_state.world);
+        lytephysics_state.world = NULL;
+    }
+    return 0;
+}
+
+int lyte_set_physics_engine(lyte_PhysicsEngineState state) {
+    lytephysics_state.enginestate = state;
+    switch (state) {
+        case LYTE_PHYSICSENGINESTATE_2D: {
+            printf("SET PHYSICS ENGINE (2D)\n");
+
+        } break;
+        case LYTE_PHYSICSENGINESTATE_3D: {
+            printf("SET PHYSICS ENGINE (3D)\n");
+
+        } break;
+        case LYTE_PHYSICSENGINESTATE_OFF: {
+            printf("SET PHYSICS ENGINE (OFF)\n");
+
+        } break;
+    }
     return 0;
 }
